9d6c8ea3843fa498338bbcfa8be704c171ca24fe
David Walker <david@davidwalker.org>
2020-04-23 15:36:12 +0100

    Add basic template for closcad model project

---
+++ src/part.clj
@@ -0,0 +1,18 @@
+(ns part
+  (:require
+   [closcad.core :as scad]
+   [closcad.utils :as u]))
+
+(def globals
+  {:$fa 5
+   :$fs 0.7})
+
+(do
+  (defn my-part []
+    [:cube 20])
+
+  (def everything [globals [my-part]])
+
+  (u/write "output/part.scad" everything)
+
+  #_(u/to-stl "output/part.stl" everything))
